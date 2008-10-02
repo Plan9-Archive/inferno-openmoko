c37a42537a09dc33a2fa4723b39f3028baf132a1
4239512 <4239512>
2008-10-02 10:33:16 +0000

    mkdir, tofix
    git-svn-id: svn://localhost/tmp/inferno-openmoko/trunk@39 9ba7f512-48e2-4bd0-b423-565c06871f8a

--- lib9/create.c
+++ lib9/create.c
@@ -23,7 +23,7 @@ create(char *f, int mode, int perm)
 	m |= O_CREAT|O_TRUNC;
 
 	if(perm & DMDIR){
-		if(mkdir(f, perm&0777) < 0)
+		if(mkdir(f, perm&0777) < 0) // bug: Nt's mkdir does not set permissions
 			return -1;
 		perm &= ~DMDIR;
 		m &= 3;
