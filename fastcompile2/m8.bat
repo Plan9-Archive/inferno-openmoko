436ddb038732c945d9b4434c1f5a2f2654a55cdd
4239512 <4239512>
2008-11-14 20:40:18 +0000

    vs
    git-svn-id: svn://localhost/tmp/inferno-openmoko/trunk@107 9ba7f512-48e2-4bd0-b423-565c06871f8a

--- fastcompile2/m8.bat
+++ fastcompile2/m8.bat
@@ -16,13 +16,14 @@ rem cl.exe -EP -DROOT="/inferno" -DKERNDATE=1666666666 -DEMU=1 -DUNICODE -D_WIN3
 rem cl.exe -c l1.c >l22
 rem link.exe l1.obj winmm.lib user32.lib ws2_32.lib gdi32.lib advapi32.lib mpr.lib
 rem cl /analyze >l
-cl /? >l
+rem cl /? >l
 rem -GAFy -GS- -Ox -Oi-
 rem -analyze
 
 rem debug
 rem -RTCc causes wm not working
-cl.exe -RTCsu -Od -Zi -Fm -MTd -WL -FC -W3 -o emu8.exe -DROOT="/inferno" -DKERNDATE=1666666666 -DEMU=1 -DUNICODE -D_WIN32_WINNT=0x0400 -DOBJTYPE=386 -Iinclude turbo.c winmm.lib user32.lib ws2_32.lib gdi32.lib advapi32.lib mpr.lib
+rem -showIncludes
+cl.exe  -RTCsu -Od -Zi -Fm -MTd -WL -FC -W3 -o emu8.exe -DROOT="/inferno" -DKERNDATE=1666666666 -DEMU=1 -DUNICODE -D_WIN32_WINNT=0x0400 -DOBJTYPE=386 -Iinclude turbo.c winmm.lib user32.lib ws2_32.lib gdi32.lib advapi32.lib mpr.lib
 exit
 
 rem release
